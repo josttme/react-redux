10c6d4431594279db73a06866a6a7ef0d44c5212
Josue Tapia <josue.tapia@live.com.ar>
2024-03-02 21:28:02 -0300

    :sparkles: [ADD] poke App

--- src/PokeApp.jsx
+++ src/PokeApp.jsx
@@ -1,50 +1,11 @@
-/* import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPokemons } from './store/slices/pokemon' */
-import { useEffect, useState } from 'react'
-import pokemons from './api/pokemons.json'
-/* import { useGetTodosQuery } from './store/apis' */
-/* import { Card } from './components/Card' */
-import { PokeCard } from './components/PokeCard'
+import { Header } from './components/Header'
+import { PokeList } from './components/PokeList'
 
 export function PokeApp() {
-	const [page, setPage] = useState(0)
-	const [poke, setPoke] = useState([])
-
-	/* 	const { data, isLoading } = useGetTodosQuery(page)
-	if (!data) return <h1>Loading...</h1>
-	const pokemons = data?.results
-	console.log('hola')
-	console.log(data) */
-	useEffect(() => {
-		setPoke(pokemons)
-	}, [])
-
-	const nextPage = () => setPage(page + 1)
-	const prevPage = () => {
-		if (page === 1) return
-		setPage(page - 1)
-	}
-
-	console.log(poke)
 	return (
 		<>
-			<h1 className="text-center text-2xl font-bold ">Poke App</h1>
-			<hr />
-			{/* <span>Loading: {isLoading ? 'True' : 'False'}</span> */}
-			{/* <Card key={pokemon.id} pokemon={pokemon} /> */}
-			<section className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5 space-y-24 bg-slate-900">
-				{poke?.map((pokemon) => (
-					<PokeCard key={pokemon.id} {...pokemon} />
-				))}
-			</section>
-			{/* {isLoading && <li>Cargando...</li>} */}
-			<button onClick={prevPage} className="m-2 border border-cyan-500 p-2">
-				Prev
-			</button>
-			<button onClick={nextPage} className="m-2 border border-cyan-500 p-2">
-				Next
-			</button>
+			<Header />
+			<PokeList />
 		</>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { App } from './App'
+import { PokeApp } from './PokeApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PokeApp />
 		</Provider>
 	</React.StrictMode>
 )
