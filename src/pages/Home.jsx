4756e25b9661ef388750c5f79e411642869f1a03
Josue Tapia <josue.tapia@live.com.ar>
2024-03-03 13:45:56 -0300

    :sparkles: [ADD] home page and layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { PokeList, BgVideo } from '../components'
+
+export function Home() {
+	return (
+		<main className="rounded-tl-3xl rounded-tr-3xl bg-[#000021]">
+			<PokeList />
+			<BgVideo />
+		</main>
+	)
+}
