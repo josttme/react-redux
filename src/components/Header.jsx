7447c06a32805bda58b074a5e2cff6347dfbca1f
Josue Tapia <josue.tapia@live.com.ar>
2024-03-04 22:12:25 -0300

    :poop: [ADD] navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
 	return (
 		<header className=" relative h-[75dvh] w-full">
@@ -9,9 +11,17 @@ export function Header() {
 						alt="Pokemon Logo"
 					/>
 				</div>
-				<h1 className="text-center text-5xl font-bold text-white/90 ">
+				<h1 className="text-center text-5xl font-bold text-white/90  ">
 					Poke App
 				</h1>
+				<nav className="fixed left-0 right-0 top-0 mx-auto mt-1 flex h-12 w-full max-w-5xl items-center justify-center rounded-full bg-[#000021]/80 ">
+					<NavLink
+						to="/favorites"
+						className="text-2xl font-bold text-[#0080ff]"
+					>
+						Favorites
+					</NavLink>
+				</nav>
 			</section>
 		</header>
 	)
