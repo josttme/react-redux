d0af145bb256cb8e011a6e81317e2a81126c3476
Josue Tapia <josue.tapia@live.com.ar>
2024-03-05 21:27:36 -0300

    :rocket: [ADD] pokemon image not found

--- src/components/pokeCard/PokeImage.jsx
+++ src/components/pokeCard/PokeImage.jsx
@@ -4,12 +4,17 @@ import { SvgPokeBall } from '../icons'
 
 export function PokeImage({ image }) {
 	const [loaded, setLoaded] = useState(false)
+	const defaultImage =
+		'https://res.cloudinary.com/josttme/image/upload/f_auto,q_auto/v1/Projects/poke/video/mfyzecql5xbqcpd2jurw'
+	const src = image || defaultImage
+
 	return (
 		<div className=" relative mx-auto -mb-5 -mt-28 grid h-80 w-full content-end justify-center  ">
 			<img
-				src={image}
+				src={src}
 				className={`${loaded && 'opacity-100'} w-full opacity-0 drop-shadow-lg saturate-[1.5] transition-all duration-200 ease-in-out group-hover:-translate-y-5 group-hover:scale-110`}
 				onLoad={() => setLoaded(true)}
+				alt="Pokemon Image"
 			/>
 			{!loaded && (
 				<div
@@ -23,5 +28,5 @@ export function PokeImage({ image }) {
 }
 
 PokeImage.propTypes = {
-	image: PropTypes.string.isRequired
+	image: PropTypes.string
 }
