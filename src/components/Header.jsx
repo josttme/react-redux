a71c0a30f79cc9c8131ca049ef5fa7cd1f949fbb
Josue Tapia <josue.tapia@live.com.ar>
2024-03-06 15:02:04 -0300

    :sparkles: [ADD] project description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,59 @@ import { NavBar } from './NavBar'
 
 export function Header() {
 	return (
-		<header className=" relative h-[75dvh] w-full">
-			<section className="relative z-20">
-				<div className=" mx-auto mt-10 max-w-lg p-5 md:ml-20">
-					<img
-						className="w-full drop-shadow-lg"
-						src="/pokemon-logo.svg"
-						alt="Pokemon Logo"
-					/>
-				</div>
-				<h1 className="text-center text-5xl font-bold text-[#eae8e5]">
-					Poke App
-				</h1>
-				<NavBar />
+		<header className="mb-10 w-full lg:h-[76dvh] lg:pt-5">
+			<NavBar />
+			<div className=" mx-auto  mt-52 max-w-lg p-5 md:ml-20 lg:mt-10">
+				<img
+					className="w-full drop-shadow-lg"
+					src="/pokemon-logo.svg"
+					alt="Pokemon Logo"
+				/>
+			</div>
+			<section className="max-w-4xl space-y-5 rounded-3xl bg-[#000021] px-5 py-6 text-[#eae8e5] lg:ml-5 lg:px-10">
+				<h1 className="text-center text-4xl font-bold ">PokeRedux</h1>
+				<p className="text-lg">
+					Explora el mundo Pokémon con esta aplicación web, desarrollada con
+					ReactJS y Redux. Accede a la información de los Pokémon sin necesidad
+					de realizar una nueva solicitud a la API, navega por la lista de forma
+					fácil y rápida, guarda tus favoritos.
+				</p>
+				<section className="flex flex-col justify-between space-y-5 md:flex-row ">
+					<ul className="list-inside list-disc text-lg marker:text-[#0080ff]">
+						Tecnologías utilizadas:
+						<li>ReactJS</li>
+						<li>Tailwind</li>
+						<li>Redux</li>
+						<li>RTK Query</li>
+					</ul>
+					<ul className="list-inside list-disc text-lg marker:text-[#0080ff]">
+						Funcionalidades principales:
+						<li>Manejo de estado con Redux</li>
+						<li>Cacheo de la API con RTK Query</li>
+						<li>Guardar favoritos en local storage</li>
+					</ul>
+					<ul className="list-inside list-disc text-lg marker:text-[#0080ff]">
+						Propósito del proyecto:
+						<li>Practicar Redux</li>
+					</ul>
+				</section>
+				<a
+					href="https://github.com/josttme/react-redux"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="inline-flex h-12 w-52  items-center justify-center gap-x-3 rounded-lg bg-gray-800 px-4 py-2 text-sm text-white duration-300 hover:bg-gray-700 "
+				>
+					<span>Gitub Code</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className="h-5 w-5"
+					>
+						<path d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+					</svg>
+				</a>
 			</section>
 		</header>
 	)
