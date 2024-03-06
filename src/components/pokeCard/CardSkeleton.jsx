d1f2e76fb0fa49a144ccb8c0b202849d52be1306
Josue Tapia <josue.tapia@live.com.ar>
2024-03-05 21:28:27 -0300

    :lipstick: [UPDATE] resposive CardSkeleton

--- src/components/pokeCard/CardSkeleton.jsx
+++ src/components/pokeCard/CardSkeleton.jsx
@@ -2,7 +2,7 @@ import { SvgPokeBall } from '../icons'
 
 export function CardSkeleton() {
 	return (
-		<div className=" grid h-[27rem] cursor-pointer gap-4 self-end rounded-3xl  border border-transparent bg-[#00388f] pb-3 ">
+		<div className=" grid h-[28rem] cursor-pointer gap-4 self-end rounded-3xl  border border-transparent bg-[#00388f] pb-3 ">
 			<div className="mx-auto  -mb-5 -mt-28 grid h-80 w-full place-content-center ">
 				<SvgPokeBall className="w-full animate-pulse self-center " />
 			</div>
