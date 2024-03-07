3ac427e015e38d88143a30c5cec74561b2ee27f5
Josue Tapia <josue.tapia@live.com.ar>
2024-03-07 00:19:03 -0300

    :wrench: [ADD] colors and grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				brand: {
+					primary: '#000021'
+				},
+				blue: {
+					primary: '#00388f',
+					secondary: '#0080ff'
+				},
+				red: {
+					heart: '#ff234e'
+				},
+				'text-color': '#eae8e5'
+			},
+			gridTemplateColumns: {
+				card: 'repeat(auto-fit, minmax(300px, 320px))'
+			}
+		}
 	},
 	plugins: []
 }
