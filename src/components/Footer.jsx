f3b01136d18107237f5c16d5534d5e2b135abc66
Josue Tapia <josue.tapia@live.com.ar>
2024-03-07 00:32:56 -0300

    :lipstick: [CHANGE] Tailwind config colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export function Footer() {
 	return (
-		<footer className="bg-[#000021] py-10  text-center lg:pl-4 ">
+		<footer className="bg-brand-primary py-10  text-center lg:pl-4 ">
 			<a
 				href="http://josu.dev"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="  text-white/80 underline underline-offset-1"
+				className="  text-white/80 underline-offset-1 lg:hover:underline"
 			>
 				© 2024 JosttMe
 			</a>
